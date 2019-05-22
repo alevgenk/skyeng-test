3676e8654f9378d1984032e27af4fed0c177e11a
Egor Ushakov <egor.ushakov.ru@gmail.com>
2019-05-22 17:55:08 +0300

    Add basic test
    (cherry picked from commit 4331a2b87e77fc36b744a88b24c942ee8646c26f)

---
+++ tests/DecoratorManagerTest.php
@@ -0,0 +1,83 @@
+<?php declare(strict_types=1);
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Cache\CacheItemInterface;
+use Psr\Cache\CacheItemPoolInterface;
+use Psr\Log\LoggerInterface;
+use ReflectionException;
+use src\Decorator\DecoratorManager;
+
+class DecoratorManagerTest extends TestCase
+{
+    /**
+     * Test manager behavior if valid cache item acquired from cache
+     */
+    public function testGetResponseWithValidCacheItem(): void
+    {
+        try {
+            $cacheItem = $this
+                ->createMock(CacheItemInterface::class);
+            $cacheItem->method('isHit')
+                ->willReturn(true);
+            $cacheItem
+                ->expects($this->once())
+                ->method('get');
+            $cache = $this->getCacheWithCacheItem($cacheItem);
+            $logger = $this->createMock(LoggerInterface::class);
+            $manager = new DecoratorManager('host', 'user', 'password', $cache, $logger);
+            $manager->getResponse([]);
+        } catch (ReflectionException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    private function getCacheWithCacheItem($item)
+    {
+        try {
+            $cache = $this
+                ->getMockForAbstractClass(CacheItemPoolInterface::class);
+            $cache->method('getItem')->willReturn($item);
+            return $cache;
+        } catch (ReflectionException $e) {
+            echo $e->getMessage();
+            return null;
+        }
+    }
+
+    /**
+     * If we get an invalid cache item from the cache
+     * do not call get() method on it
+     * but
+     */
+    public function testGetResponseWithInValidCacheItem(): void
+    {
+        try {
+            $cacheItem = $this
+                ->getMockBuilder(CacheItemInterface::class)
+                ->setMethods(['isHit', 'get', 'set', 'expiresAt'])
+                ->getMockForAbstractClass();
+            $cacheItem->method('isHit')
+                ->willReturn(false);
+            $cacheItem->method('set')->willReturn($cacheItem);
+
+            $cacheItem
+                ->expects($this->never())
+                ->method('get');
+            $cacheItem
+                ->expects($this->once())
+                ->method('set');
+            $cacheItem
+                ->expects($this->once())
+                ->method('expiresAt');
+            $cache = $this->getCacheWithCacheItem($cacheItem);
+            $logger = $this->createMock(LoggerInterface::class);
+            $manager = new DecoratorManager('host', 'user', 'password', $cache, $logger);
+            $manager->getResponse([]);
+        } catch (ReflectionException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+}
